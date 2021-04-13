a1a65201e716ee4764a5f45875b6bf425e91e1ae
Sarthak Verma <64399925+SarthakVerma26@users.noreply.github.com>
2021-04-13 12:46:22 +0530

    Create wordprint.c

---
+++ 3_Implementation/src/wordprint.c
@@ -0,0 +1,19 @@
+void wordprint(char* s,int x,int y)   
+{ 
+	char *p;
+
+	int count=0;
+	int i;
+	while(*s!=NULL)
+	{
+		s++;
+		count++;
+	}
+	s-=count;
+	for(i=0;i<count;i++){
+		p=_vp+(y*_width+x+i)*(_color_bits/8);
+		*p=*(s+i);
+		*(p+1)=YELLOW;
+	}
+	
+}
