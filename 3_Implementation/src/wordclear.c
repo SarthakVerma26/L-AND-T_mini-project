cc04962545496dfc960b090f9802085796736f87
Sarthak Verma <64399925+SarthakVerma26@users.noreply.github.com>
2021-04-13 12:47:22 +0530

    Create wordclear.c

---
+++ 3_Implementation/src/wordclear.c
@@ -0,0 +1,12 @@
+void wordclear(int i)    
+{
+	int j=0;
+	char *p;
+	while(*(wordlib[i].s+j)!=NULL)
+	{
+		p=_vp+(wordlib[i].y*_width+wordlib[i].x+j)*(_color_bits/8);
+		*p=NULL;
+		*(p+1)=NULL;
+		j++;
+	}
+}
